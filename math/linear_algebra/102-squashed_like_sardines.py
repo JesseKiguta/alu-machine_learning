82bdc9ac8eacd1c502a435c515f647c9c9eaebb0
Jesse Kiguta <j.kiguta@alustudent.com>
2024-09-14 12:50:19 -0700

    102

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
-'''
-Function that joins two matrices to create a new one
-'''
-import numpy as np
+""" defines function that concatenates two matrices along a specific axis """
+
+
+def matrix_shape(matrix):
+    """ returns list of integers representing dimensions of given matrix """
+    matrix_shape = []
+    while (type(matrix) is list):
+        matrix_shape.append(len(matrix))
+        matrix = matrix[0]
+    return matrix_shape
 
 
 def cat_matrices(mat1, mat2, axis=0):
-    '''
-    Concatenates two matrices along a specific axis
-    '''
-    try:
-        arr1 = np.array(mat1)
-        arr2 = np.array(mat2)
-        
-        result = np.concatenate((arr1, arr2), axis=axis)
-        
-        return result.tolist()
-    except ValueError:
+    """ returns concatenation of two matrices along a specific axis """
+    from copy import deepcopy
+    shape1 = matrix_shape(mat1)
+    shape2 = matrix_shape(mat2)
+    if len(shape1) != len(shape2):
         return None
+    for i in range(len(shape1)):
+        if i != axis:
+            if shape1[i] != shape2[i]:
+                return None
+    return rec(deepcopy(mat1), deepcopy(mat2), axis, 0)
+
+
+def rec(m1, m2, axis=0, current=0):
+    """ recursively calls function until gets to level to extend """
+    if axis != current:
+        return [rec(m1[i], m2[i], axis, current + 1) for i in range(len(m1))]
+    m1.extend(m2)
+    return m1
