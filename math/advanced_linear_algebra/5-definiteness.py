c847f8897f95ac438212cce198303f31f17ca720
Jesse Kiguta <j.kiguta@alustudent.com>
2024-09-21 07:02:14 -0700

    definite

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Function that evaluates the definiteness of a matrix
+"""
+
+import numpy as np
+
+
+def definiteness(matrix):
+    """
+    Computes eigenvalues of suitable matrices, returns definiteness
+    """
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+
+    if len(matrix.shape) < 2:
+        return None
+
+    if matrix.shape[0] != matrix.shape[1]:
+        return None
+
+    eigenvalues = np.linalg.eigvals(matrix)
+
+    if all (eigenvalues > 0):
+        return "Positive definite"
+    elif all (eigenvalues >= 0):
+        return "Positive semi-definite"
+    elif all (eigenvalues < 0):
+        return "Negative definite"
+    elif all (eigenvalues <= 0):
+        return "Negative semi-definite"
+    else:
+        return "Indefinite"
