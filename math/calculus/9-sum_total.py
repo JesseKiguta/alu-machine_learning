7d9bf30e94a85a2d8c238e0bd3ab4d302ab476a3
Jesse Kiguta <j.kiguta@alustudent.com>
2024-09-30 07:55:01 -0700

    debug 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
+"""
+Module containing function that sums squares from i to n
+"""
+
+
 def summation_i_squared(n):
-    if type(n) is not int:
+    """
+    Uses summation of squares formula to add squares upto n
+    """
+    if type(n) is not int or n < 0:
         return None
     else:
-        return int ((n * (n + 1) * (2 * n + 1))/ 6)
+        return int((n * (n + 1) * (2 * n + 1)) / 6)
