777b61d5004724ff4adede1e65cd46c90d4901a0
Jesse Kiguta <j.kiguta@alustudent.com>
2024-10-14 05:04:52 -0700

    first task

---
+++ math/multivariate_prob/0-mean_cov.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+mean and covariance
+'''
+import numpy as np
+
+
+def mean_cov(X):
+    '''
+    Finds the mean and covariance of a dataset X
+    '''
+    if X.ndim != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+
+    n = X.shape[0]
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+
+    mean_array = np.mean(X, axis = 0)
+
+    centered_data = X - mean_array
+    cov_matrix = (centered_data.T @ centered_data) / (n - 1)
+
+    return mean_array , cov_matrix
