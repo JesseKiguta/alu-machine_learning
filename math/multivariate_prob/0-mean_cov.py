80f841112dc95286da3f2560666c9d561ebdae34
Jesse Kiguta <j.kiguta@alustudent.com>
2024-10-14 05:50:27 -0700

    debug 0

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -9,16 +9,16 @@ def mean_cov(X):
     '''
     Finds the mean and covariance of a dataset X
     '''
-    if X.ndim != 2:
+    if len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
 
     n = X.shape[0]
     if n < 2:
         raise ValueError("X must contain multiple data points")
 
-    mean_array = np.mean(X, axis = 0)
+    mean_array = np.mean(X, axis=0).reshape(1, -1)
 
     centered_data = X - mean_array
     cov_matrix = (centered_data.T @ centered_data) / (n - 1)
 
-    return mean_array , cov_matrix
+    return mean_array, cov_matrix
