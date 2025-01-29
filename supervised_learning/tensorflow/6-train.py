628beba7a13c061349178e8698dcb4ab98f1a9b3
JesseKiguta <j.kiguta@alustudent.com>
2025-01-29 15:19:06 +0200

    fix imports in number 6

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -3,12 +3,11 @@
 
 
 import tensorflow as tf
-calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 create_placeholders = __import__('0-create_placeholders').create_placeholders
-create_train_op = __import__('5-create_train_op').create_train_op
-calculate_loss = __import__('2-calculate_loss').calculate_loss
 forward_prop = __import__('2-forward_prop').forward_prop
-
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_train_op = __import__('5-create_train_op').create_train_op
 
 def train(X_train, Y_train, X_valid, Y_valid,
           layer_sizes, activations, alpha, iterations,
