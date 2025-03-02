4bcfe78f67389093b0529ee7444ed1585b930093
JesseKiguta <j.kiguta@alustudent.com>
2025-03-02 15:26:44 +0200

    234 again

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python3
-"""Script that prints the location of a specific user"""
+"""
+Uses the GitHub API to print the location of a specific user,
+where user is passed as first argument of the script with full API URL
+
+ex) "./2-user_location.py https://api.github.com/users/holbertonschool"
+"""
+
 
 import requests
-import sys
-import time
+from sys import argv
+from time import time
 
 
 if __name__ == "__main__":
-    res = requests.get(sys.argv[1])
-
-    if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
-        current_time = int(time.time())
-        diff = int((rate_limit - current_time) / 60)
-        print('Reset in {} min'.format(int(diff)))
-
-    elif res.status_code == 404:
-        print("Not found")
-    elif res.status_code == 200:
-        res = res.json()
-        print(res['location'])
+    if len(argv) < 2:
+        raise TypeError(
+            "Input must have the full API URL passed in as an argument: {}{}".
+            format('ex. "./2-user_location.py',
+                   'https://api.github.com/users/holbertonschool"'))
+    try:
+        url = argv[1]
+        results = requests.get(url)
+        if results.status_code == 403:
+            reset = results.headers.get('X-Ratelimit-Reset')
+            waitTime = int(reset) - time()
+            minutes = round(waitTime / 60)
+            print('Reset in {} min'.format(minutes))
+        else:
+            results = results.json()
+            location = results.get('location')
+            if location:
+                print(location)
+            else:
+                print('Not found')
+    except Exception as err:
+        print('Not found')
